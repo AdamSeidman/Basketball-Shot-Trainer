79d7161740429027ae4812e07bb7cf30e5534827
fussmj <fuss.matthew.j@gmail.com>
2021-10-29 08:28:03 -0400

    object detection method

--- Code/Python_Code/main.py
+++ Code/Python_Code/main.py
@@ -3,16 +3,43 @@ Created on Sep 23, 2021
 
 @author: Matthew
 '''
+import numpy as np
+import cv2
 import User_Classes as uc
+import Image_Processing_Classes as ip
+from matplotlib import pyplot as plt
 
-coach_Ellis = uc.User('John Ellis')
 
-matthew = uc.Player('Matthew', 'Fuss', 30)
-player2 = uc.Player('Matt', 'F', 30)
 
-if __name__ == '__main__':
-    coach_Ellis.add_player(matthew)
-    coach_Ellis.add_player(player2)
-    print(coach_Ellis.players[0].first_name)
-    print(coach_Ellis.players[1].first_name)
-    pass
+img = cv2.imread("two_balls.png")
+img = cv2.imread("image1.png")
+#cap = cv2.VideoCapture(0)
+
+
+classNames = []
+
+classFile = 'coco.names'
+
+""" with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+print(classNames) """
+
+configPath = ('ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
+weightsPath = ('frozen_inference_graph.pb')
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+classIds, confs, bbox = net.detect(img, confThreshold = 0.1)
+#print(classIds, bbox)
+if len(classIds) != 0:
+    for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+        if classId == 34:
+            cv2.rectangle(img, box, color = (0, 255, 0), thickness = 2)
+
+cv2.imshow("Output", img)
+cv2.waitKey(0)
